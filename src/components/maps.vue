504a1e40677e1a3c7e7d80184fcbafde10a251de
Sean Price <seanparade@gmail.com>
2019-11-12 19:35:05 -0500

    stripped out linter, refactored maps component a bit.

--- src/components/maps.vue
+++ src/components/maps.vue
@@ -1,35 +1,36 @@
 <template>
     <div id="map"></div>
 </template>
-
 <script>
+    //prettier-ignore
+    function initMap (){
+
+        var myOptions = {
+            zoom: 2,
+            center: new window.google.maps.LatLng(39.9388838,116.3974589),
+        };
+        var map = new window.google.maps.Map(document.getElementById("map"), myOptions);
+        let script = document.createElement('script')
+        script.async = true;
+        script.defer = true;
+        script.src = '';
+        document.getElementsByTagName('head')[0].appendChild(script);
+        console.log('appended')
+    }
+
+    console.log(initMap)
+
     export default {
         name: "maps",
-        mounted() {
-            this.loadGmap();
-        },
         methods: {
-            loadGmap: function(){
-                let script = document.createElement('script')
-                script.async = true;
-                script.defer = true;
-                script.src = 'https://maps.googleapis.com/maps/api/js?key=$AIzaSyDTFd6UTZ6sUOcSRLrRCPCEOAYmfrEz-Zg&callback=mapInit';
-                window.mapinit = () => {
-                    document.getElementsByTagName('head')[0].appendChild(script);
-                    console.log('appended')
-                }
-            this.createMap();
-            },
-            createMap: function(){
-                // If I include below, I get 'google' is not defined error which makes me think that the script is not loaded? 
-                /*
-                var myOptions = {
-                    zoom: 2,
-                    center: new google.maps.LatLng(39.9388838,116.3974589),
-                };
-                var map = new google.maps.Map(document.getElementById("map"), myOptions);
-                */
-            }
-        }
+
+        },
+        mounted() {
+            let mapScript = document.createElement('script')
+            mapScript.async = true;
+            mapScript.defer = true;
+            mapScript.src = 'https://maps.googleapis.com/maps/api/js?key=$AIzaSyDTFd6UTZ6sUOcSRLrRCPCEOAYmfrEz-Zg&callback=initMap';
+            this.$el.parentElement.append(mapScript)
+        },
     }
 </script>
